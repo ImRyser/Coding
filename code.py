7ab7751c8854d1d1f7163d3087c91114945f4340
ryser <rysercooksey67@gmail.com>
2026-03-13 16:12:30 +1300

    Starting code

--- code.py
+++ code.py
@@ -1,9 +1,23 @@
-from tkinter import *
+from tkinter import*
+import random
 
 def quit():
-    main_window.destroy()
+    main_window.destroy
 
 def entry_print():
-    Label(main_window,text= entry_first_name.get()).grid(column=0,row=3,sticky=E)
-    Label(main)
-print("hello")
+    Label(main_window,text= entry_full_name.get()).grid(column=0,row=2,sticky=E)
+
+def generate_random():
+    random_number = random.randint(1,10)
+    Label(main_window,text=random_number).grid(column=0,row=2, sticky=E)
+
+def main():
+    Button(main_window, text="Quit",command= quit) .grid(column=0,row=1)
+    Button(main_window, text="Print",command=entry_print).grid(column=1,row=1)
+    Label(main_window,text="Full Name").grid(column=0,row=2)
+    main_window.mainloop()
+
+main_window =Tk()
+entry_full_name= Entry(main_window)
+entry_full_name.grid(column=2,row=2,padx=10,pady=5)
+main()

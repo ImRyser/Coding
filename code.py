e4a1b2bc960bbfa6264cf9231fbd6aab63795164
ryser <rysercooksey67@gmail.com>
2026-03-16 14:24:43 +1300

    adding print statements

--- code.py
+++ code.py
@@ -5,7 +5,11 @@ def quit():
     main_window.destroy
 
 def entry_print():
-    Label(main_window,text= entry_full_name.get()).grid(column=0,row=2,sticky=E)
+    Label(main_window,text= entry_full_name.get()).grid(column=0,row=6,sticky=E)
+    Label(main_window,text=entry_Receipt_number.get()).grid(column=1,row=6)
+    Label(main_window,text=entry_item_hired.get()).grid(column=2,row=6)
+    Label(main_window,text=entry_number_hired.get()).grid(column=3,row=6)
+
 
 def generate_random():
     random_number = random.randint(1,10)
@@ -14,10 +18,14 @@ def generate_random():
 def main():
     Button(main_window, text="Quit",command= quit) .grid(column=0,row=1)
     Button(main_window, text="Print",command=entry_print).grid(column=1,row=1)
-    Label(main_window,text="Full Name").grid(column=0,row=2)
+    Label(main_window,text="Customer Name").grid(column=0,row=2)
+    Label(main_window,text="Receipt Number").grid(column=0,row=3)
+    Label(main_window,text="Item Hired").grid(column=0,row=4)
+    Label(main_window,text="Number Hired").grid(column=0,row=5)
     main_window.mainloop()
 
 main_window =Tk()
 entry_full_name= Entry(main_window)
 entry_full_name.grid(column=2,row=2,padx=10,pady=5)
+
 main()
